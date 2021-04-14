e295c7d66d061ace032d56a6b341bdff3372455a
Zondazx <mzxgd16@gmail.com>
2021-04-14 18:48:59 -0400

    feat: Layout component, global and util stylesheet and static assets

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,20 @@
+import Link from 'next/link';
+import Head from 'next/head';
+
+import Layout from '../../components/Layout';
+
 export default function FirstPost() {
-  return <h2>My first next.js post</h2>
+  return (
+    <Layout>
+    <Head>
+      <title>My first post</title>
+    </Head>
+
+      <h2>My first next.js post</h2>
+
+      <Link href='/'>
+        <a>Home</a>
+      </Link>
+    </Layout>
+  );
 };
